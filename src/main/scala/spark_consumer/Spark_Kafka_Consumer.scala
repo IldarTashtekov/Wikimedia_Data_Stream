809d5dd8f1ea545c7b5ab20701852bf84be76249
a <a>
2023-01-11 19:45:08 +0100

    comented code

--- src/main/scala/spark_consumer/Spark_Kafka_Consumer.scala
+++ src/main/scala/spark_consumer/Spark_Kafka_Consumer.scala
@@ -3,8 +3,6 @@ package spark_consumer
 
 import org.apache.spark.streaming.kafka010._
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
-import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession.setActiveSession
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
@@ -17,15 +15,16 @@ import scala.reflect.io.File
 object Spark_Kafka_Consumer {
 
 
-
+  //spark session
   val spark: SparkSession = SparkSession.builder()
     .appName("Spark Kafka Consumer")
     .master("local[2]")
     .getOrCreate()
 
-
+  //streaming context to use spark streaming
   val ssc = new StreamingContext(spark.sparkContext, Seconds(1))
 
+  //kafka params
   val kafkaParams:Map[String,Object]=Map(
     "bootstrap.servers" -> "127.0.0.1:9092",
     "key.serializer" -> classOf[StringSerializer], // send data to kafka
@@ -36,18 +35,24 @@ object Spark_Kafka_Consumer {
     "enable.auto.commit" -> false.asInstanceOf[Object]
   )
 
+  //kafka topic to consume on the same kafka topic where the kafka producer is sending data
   val kafkaTopic = "wikimedia.recentchange"
 
+  //a map when we save the domain and the number of apperances of that domain
   var dom_map:scala.collection.mutable.Map[String,Int]=mutable.Map()
 
+  //a schema to create a df from rdds and save in csv later
   val domMapSchema: StructType =StructType(Array(
     StructField("Domain", StringType),
     StructField("Number_of_appearances ", IntegerType)
   ))
 
+  //the path of the csv when we gonna save the domain and its number of appearances
   val csvPath="src/main/scala/spark_consumer/domains_csv"
 
+
   def readFromKafka() = {
+
     val topics = Array(kafkaTopic)
     val kafkaDStream = KafkaUtils.createDirectStream(
       ssc,
@@ -66,33 +71,30 @@ object Spark_Kafka_Consumer {
        */
     )
 
-
+    //raw kafka stream, receive a integer key and string value, the value is a json with information about new articles in wikimedia
     val processedStream = kafkaDStream.map(record =>(record.key(),record.value()))
 
+    //we only need the domain, for this reason we gonna do some transformations
     val atributesStream = processedStream.map{line=>
 
+      //first we gonnna transform a string with json in json4s.JValue object to get data in more easy way
       val jsonString= s"""${line._2}"""
       val jsonValues = parseJson(jsonString)
 
+      //then get the value we searching
       val domain = jsonValues\"meta"\"domain"
-      /*val uri = jsonValues\"meta"\"uri"
-      val pageTitle = jsonValues\"page_title"
-      val revLen = jsonValues\"rev_len"
-      val revDate = jsonValues\"rev_timestamp"
 
-      new WikimediaNewArticle(domain.values.toString,
-                              uri.values.toString,
-                              pageTitle.values.toString,
-                              revLen.values.toString,
-                              revDate.values.toString
-      )*/
-
+      //every iteration returns a string with the domain name
       domain.values.toString
     }
 
+    //we gonna iterate the stream with domains
+    //RDD[String]
     atributesStream.foreachRDD{ rdd =>
+      //String
       rdd.foreach{ dom =>
 
+          //if domain exist then we sum 1 to value if not we add the domain with value 1
           if(dom_map.keys.exists(_==dom)){
 
             dom_map(dom)+=1
@@ -108,27 +110,48 @@ object Spark_Kafka_Consumer {
      }
     }
 
+    //the stream starts
     ssc.start()
 
 
+    //the stream gonna be active for 1 minute
     try TimeUnit.MINUTES.sleep(1)
     catch {
       case e: InterruptedException =>
         e.printStackTrace()
     }
-
+    //stream context stops but spark context still active becouse we gonna use it to save
+    //the map information to csv
     ssc.stop(false)
 
   }
 
+
+  def saveCSV(): Unit ={
+
+    //Map[String, Int] => Seq[Row[String,Int]]
+    val rows_seq = dom_map.map(dom=>Row(dom._1,dom._2)).toSeq
+    //Seq[Row] => RDD[Row]
+    val rowRDDs = spark.sparkContext.parallelize(rows_seq)
+    //RDD[Row] => DataFrame
+    val saveCSV_DF = spark.createDataFrame(rowRDDs,domMapSchema)
+    //show the DF in terminal
+    saveCSV_DF.show()
+    //here we save the dataframe to csv
+    saveCSV_DF.write.mode("overwrite").csv(csvPath)
+  }
+
+
   def loadCSV(): Unit ={
-    if(File(csvPath).exists){
+
+    //we load the csv as DF
       val loadCSV_DF = spark.read
         .schema(domMapSchema)
         .csv(csvPath)
 
       loadCSV_DF.show()
 
+      //we get the data of every row and put it inside the map
       loadCSV_DF.foreach{row=>
 
         val domain:String = row(0).asInstanceOf[String]
@@ -137,31 +160,20 @@ object Spark_Kafka_Consumer {
 
         //explicitly returns Unit
         ()
-
       }
 
       dom_map.foreach(print)
-
-    }else{
-      println("CSV dont exist, probably this is the first run, if it is not something dont work well")
-    }
-
-
   }
-
-  def saveCSV(): Unit ={
-
-    val rows_seq = dom_map.map(dom=>Row(dom._1,dom._2)).toSeq
-    val rowRDDs = spark.sparkContext.parallelize(rows_seq)
-    val saveCSV_DF = spark.createDataFrame(rowRDDs,domMapSchema)
-    saveCSV_DF.write.mode("overwrite").csv(csvPath)
-  }
-
 
   def main(args: Array[String]): Unit = {
 
-    loadCSV()
+    //is csv file exists we load the csv
+    if(File(csvPath).exists) loadCSV()
+
+    //our kafka streaming when we compile the data about domain appearances
     readFromKafka()
+
+    //here we save the csv to some data persistence
     saveCSV()
 
   }
